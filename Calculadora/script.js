0695eff2f67e818b685051f149ddf46096d052ea
lucascorrea <lucascorrea.ads01@gmail.com>
2021-09-23 16:41:23 -0300

    Atualização do dia 23/09/2021

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,11 +1,17 @@
-let coefA = document.getElementById('coefA')
-let coefB = document.getElementById('coefB')
-let coefC = document.getElementById('coefC')
-let res = document.getElementById('res')
+const coefA = document.getElementById('coefA')
+const coefB = document.getElementById('coefB')
+const coefC = document.getElementById('coefC')
+const res = document.getElementById('res')
 let x1 = 0
 let x2 = 0
 
-function detectorErros(detector){
+document.addEventListener("keypress", function (e) {
+    if (e.key === 'Enter') {
+        calcular()
+    }
+})
+
+function detectorErros() {
     if (coefA.value.length == 0 || coefB.value.length == 0 || coefC.value.length == 0) {
         alert('[ERROR] Valores inválidos')
         return true
@@ -21,17 +27,17 @@ function calcular(event) {
 }
 
 function bhaskara(a, b, c) {
-    
+
     a = Number.parseInt(coefA.value)
     b = Number.parseInt(coefB.value)
     c = Number.parseInt(coefC.value)
-   
-    let delta = (b**2) - (4 * a * c)
+
+    const delta = (b ** 2) - (4 * a * c)
     if (delta < 0) {
         res.innerHTML = 'A equação não possui raiz real'
     } else {
-        x1 = ((-b + Math.sqrt(delta)) / (2*a)).toFixed(4)
-        x2 = ((-b - Math.sqrt(delta)) / (2*a)).toFixed(4)
+        x1 = (-b + Math.sqrt(delta)) / (2 * a)
+        x2 = (-b - Math.sqrt(delta)) / (2 * a)
     }
-    
+
 }
